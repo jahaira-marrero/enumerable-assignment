b0b9bfe3316fb0b2996a5f78bdc76e3db0a5a335
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-29 14:45:19 -0500

    done

--- run.rb
+++ run.rb
@@ -29,54 +29,45 @@ def instructors
     ]
 end
 
-
-
 def all_names
-
+    instructors.map { |instructor| instructor[:name]}
 end
-
 
 
 def name_and_role
-
+    instructors.map {|hash| {hash[:name] => hash[:role]}}
+   binding.pry
 end
-
-
 
 def all_coaches
-
+    instructors.select { |instructor| instructor[:role] == "Coach"}
 end
-
-
 
 def all_leads
-
+    instructors.select { |instructor| instructor[:role] == "Lead"}
 end
 
-
-
 def first_instructor_with_a_car
-
+    instructors.find { |instructor| instructor[:owns_car]}
 end
 
 
 
 def instructors_with_more_than_1_sibling
-
+    instructors.select { |instructor| instructor[:siblings] > 1}
 end
 
 
 
 def most_siblings
-
+    instructors.select.max { |instructor| instructor[:siblings]}
 end
 
 
 
 def total_siblings
-
+    instructors.select.sum { |instructor| instructor[:siblings]}
 end
-
 
 
 
